d7c9dcb8330c9421d1e4be8ec9f148f05bfbb6e6
Birkan Sarıbacak <birkan35@hotmail.com.tr>
2025-12-07 21:30:53 +0300

    final.

--- src/main/java/com/workintech/s18d1/dao/BurgerDao.java
+++ src/main/java/com/workintech/s18d1/dao/BurgerDao.java
@@ -1,8 +1,6 @@
 package com.workintech.s18d1.dao;
-
 import com.workintech.s18d1.entity.BreadType;
 import com.workintech.s18d1.entity.Burger;
-
 import java.util.List;
 
 public interface BurgerDao {
